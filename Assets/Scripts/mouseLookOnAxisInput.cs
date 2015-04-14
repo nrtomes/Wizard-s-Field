4f67cc6666a28fae64715168f58cfe2c01335515
Nick <nrtomes@gmail.com>
2015-04-14 01:54:46 -0400

    Movement Working - Maybe?
    I'll have to play around a little more with it before I'm comfortable
    with the rotation smoothing and speeds.

---
+++ Assets/Scripts/mouseLookOnAxisInput.cs
@@ -0,0 +1,57 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class mouseLookOnAxisInput : MonoBehaviour {
+
+	public string verticalAxis = "VerticalLook";
+	public float baseRotationSpeed = 3.0f;
+	public float sensitivityMultiplier = 1.0f;
+	public bool invertVerticalAxis = false;
+	public float flipVerticalAxis = -1.0f;
+	public float verticalLimitMax = 85.0f;
+	public float verticalLimitMin = -85.0f;
+	public float smoothTime = 3.0f;
+
+	private Quaternion cameraTargetRotation;
+
+	void Start ()
+	{
+		cameraTargetRotation = transform.localRotation;
+	}
+
+	// Update is called once per frame
+	void Update () 
+	{
+		if(invertVerticalAxis)
+		{ flipVerticalAxis = -1.0f; }
+		else
+		{ flipVerticalAxis = 1.0f; }
+
+		float vertRotation = Input.GetAxis(verticalAxis)*baseRotationSpeed*sensitivityMultiplier*flipVerticalAxis;
+	
+		cameraTargetRotation = ClampRotationAroundXAxis (cameraTargetRotation);
+
+		cameraTargetRotation *= Quaternion.Euler (-vertRotation, 0f, 0f);
+//		Debug.Log (cameraTargetRotation.eulerAngles.x);
+//		Debug.Log (vertRotation);
+
+		//transform.localRotation = cameraTargetRotation;
+		transform.localRotation = Quaternion.Slerp(transform.localRotation, cameraTargetRotation, smoothTime * Time.deltaTime);
+	}
+
+	Quaternion ClampRotationAroundXAxis(Quaternion q)
+	{
+		q.x /= q.w;
+		q.y /= q.w;
+		q.z /= q.w;
+		q.w = 1.0f;
+		
+		float angleX = 2.0f * Mathf.Rad2Deg * Mathf.Atan (q.x);
+		
+		angleX = Mathf.Clamp (angleX, verticalLimitMin, verticalLimitMax);
+		
+		q.x = Mathf.Tan (0.5f * Mathf.Deg2Rad * angleX);
+		
+		return q;
+	}
+}
---
+++ Assets/Scripts/moveOnAxisInput.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class moveOnAxisInput : MonoBehaviour 
+{
+	public string horizontalMoveAxis = "HorizontalMove";
+	public string verticalMoveAxis = "VerticalMove";
+
+	public string horizontalLookAxis = "HorizontalLook";
+	public float baseRotationSpeed = 3.0f;
+	public float sensitivityMultiplier = 1.0f;
+	public bool invertHorizontalAxis = false;
+	public float flipHorizontalAxis = -1.0f;
+	public bool smoothHorizontalRotation = true;
+	public float smoothTime = 8.0f;
+
+	public float speed = 6.0F;
+	public float jumpSpeed = 8.0F;
+	public float gravity = 20.0F;
+
+	private Quaternion playerTargetRotation = Quaternion.identity;
+	private Vector3 moveDirection = Vector3.zero;
+
+	void Update() 
+	{
+		CharacterController controller = GetComponent<CharacterController>();
+
+		if (controller.isGrounded) 
+		{
+
+			moveDirection = new Vector3(Input.GetAxis(horizontalMoveAxis), 0, Input.GetAxis(verticalMoveAxis));
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= speed;
+
+			if (Input.GetButton("Jump"))
+				moveDirection.y = jumpSpeed;
+			
+		}
+
+		if(invertHorizontalAxis) 
+		{ flipHorizontalAxis = -1.0f; }
+		else 
+		{ flipHorizontalAxis = 1.0f; }
+
+		float horizontalRotation = Input.GetAxis(horizontalLookAxis)*baseRotationSpeed*sensitivityMultiplier*flipHorizontalAxis;
+		playerTargetRotation *= Quaternion.Euler (0f, horizontalRotation, 0f);
+
+		if(smoothHorizontalRotation)
+		{
+			transform.localRotation = Quaternion.Slerp(transform.localRotation, playerTargetRotation, smoothTime * Time.deltaTime);
+		}
+		else
+		{
+			transform.localRotation = playerTargetRotation;		
+		}
+
+		moveDirection.y -= gravity * Time.deltaTime;
+
+		controller.Move(moveDirection * Time.deltaTime);
+	}
+}
