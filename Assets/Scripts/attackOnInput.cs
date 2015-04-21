9c8d73fb8730d177b1d7a18f191c88258f7a79d0
Nick <nrtomes@gmail.com>
2015-04-21 19:58:42 -0400

    Very Poor Sword Swing + Bonus attack
    Probably not the most reasonable way to deal with anything that I've
    changed but it's fun to knock blocks around with the sword.

---
+++ Assets/Scripts/attackOnInput.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class attackOnInput : MonoBehaviour 
+{
+	public string attackPrimary = "Fire1";
+	public bool isAttacking = false;
+
+	public Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetButton (attackPrimary))
+		{
+			animator.SetBool ("isAttacking", true);
+		}
+		else
+		{
+			animator.SetBool ("isAttacking", isAttacking);
+		}
+	}
+}
---
+++ Assets/addForceOnTriggerEnter.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class addForceOnTriggerEnter : MonoBehaviour {
+
+	public ForceMode forceMode;
+	public float forceStrength = 20.0f;
+	public float radius = 0.5f;
+	public float upwardsModifier = 0.0f;
+
+	void OnTriggerEnter (Collider other) 
+	{
+		other.attachedRigidbody.AddExplosionForce (forceStrength, transform.position, radius, upwardsModifier, forceMode);
+	}
+}
