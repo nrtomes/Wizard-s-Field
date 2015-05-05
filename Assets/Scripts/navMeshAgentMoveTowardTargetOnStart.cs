173b16ee276b828fefcdfa8615c1f9f5680dc072
Nick <nrtomes@gmail.com>
2015-05-05 17:32:17 -0400

    Messing around with the NavMesh
    Probably not to dangerous to add but I want to keep it out of the master
    branch for now.

---
+++ Assets/Scripts/navMeshAgentMoveTowardTargetOnStart.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class navMeshAgentMoveTowardTargetOnStart : MonoBehaviour {
+
+	public Transform target;
+
+	public NavMeshAgent agent;
+	
+	void Start () {
+		agent = GetComponent<NavMeshAgent>();
+
+	}
+
+	void FixedUpdate () {
+		agent.destination = target.position; 
+	}
+}
